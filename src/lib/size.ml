ee4730cd17440a1b03493dd655608312dc6bb6d7
Mehdi Dogguy <mehdi@debian.org>
2015-01-26 23:13:44 +0100

    Clearer code in Size module

--- src/lib/size.ml
+++ src/lib/size.ml
@@ -21,50 +21,44 @@
 open Errors
 
 type t = {
-  size: float;
-  multiplier: float;
-  base10: bool;
-  unit: float;
+  size: float;       (* Size modulo multiplier and unit        *)
+  multiplier: float; (* Helps to get back to the original form *)
+  base10: bool;      (* Only useful to not inspect multiplier  *)
+  unit: float;       (* Byte or bits, that is the question.    *)
 }
 
 let value s =
   s.size *. s.multiplier *. s.unit
 
-let rex = Pcre.regexp "^(\\d+(\\.\\d{0,2})?)\\h*([kMGTPEZY]?i?[bBo]?)$"
+let rex = Pcre.regexp "^(\\d+(\\.\\d{0,2})?)\\h*([kMGTPEZY]?i?[bB]?)$"
 
-let seq b e =
-  let rec seq b e =
-    if b = e
-    then [e]
-    else b::(seq (b+1) e)
-  in seq b e
-
-let powers = ['k'; 'M'; 'G'; 'T'; 'P'; 'E'; 'Z'; 'Y']
+let powers =
+ ['k', 1; 'M', 2; 'G', 3; 'T', 4; 'P', 5; 'E', 6; 'Z', 7; 'Y', 8]
 
 let multiplier_of_char m base10 =
-  if List.mem m powers then
-    let seq = seq 1 (List.length powers) in
-    let powers = List.combine powers seq in
+  try
     let p = float_of_int (List.assoc m powers) in
     if base10 then
       10. ** (3. *. p) 
     else
       2. ** (10. *. p)
-  else
+  with Not_found ->
     raise (Invalid_size_multiplier m)
 
 let string_of_mutlplier m base10 =
-  let seq = seq 1 (List.length powers) in
-  let powers = List.combine seq powers in
   if m = 1. then
-    ""
+    if base10 then "" else "i"
   else
-    let p, f =
+    let p, suffix =
       if base10
-      then (ceil (log m /. log 10.)) /. 3., ExtString.String.of_char
-      else (ceil (log m /. log 2.)) /. 10., (Printf.sprintf "%ci")
+      then (ceil (log m /. log 10.)) /. 3., ""
+      else (ceil (log m /. log 2.)) /. 10., "i"
     in
-    f (List.assoc (int_of_float p) powers)
+    let powers = List.map (fun (c, d) -> d, c) powers in
+    Printf.sprintf
+      "%c%s"
+      (List.assoc (int_of_float p) powers)
+      suffix
 
 let unit_of_char u =
   if u = 'b' then 1. else 8.
@@ -74,23 +68,31 @@ let char_of_unit u =
 
 let make n =
   try
-    { size = floor (float_of_string n); multiplier = 1.; base10 = false; unit = 1. }
+    let size = floor (float_of_string n) in
+    let size, unit =
+      if (int_of_float size) mod 8 = 0 then
+        size /. 8., 8.
+      else
+        size, 1.
+    in
+    { size = size; multiplier = 1.; base10 = true; unit = unit }
   with _ ->
     if Pcre.pmatch ~rex n then
       let substrings = Pcre.extract ~rex n in
       let size = float_of_string substrings.(1) in
       let unit = substrings.(3) in
-      let size, multiplier, base, unit =
-        match String.length unit with
-          | 1 -> floor size, 1., true, (if unit.[0] = 'B' || unit.[0] = 'o' then 8. else 1.)
-          | 2 -> size, multiplier_of_char unit.[0] true, true, (if unit.[1] = 'B'|| unit.[1] = 'o' then 8. else 1.)
-          | 3 -> size, multiplier_of_char unit.[0] false, false, (if unit.[2] = 'B'|| unit.[2] = 'o' then 8. else 1.)
-          | _ -> assert false
+      let size, multiplier, base10, unit =
+        match ExtString.String.explode unit with
+        | b::[]         -> size, 1., true, (if b = 'B' then 8. else 1.)
+        | 'i'::b::[]    -> size, 1., false, (if b = 'B' then 8. else 1.)
+        | m::b::[]      -> size, multiplier_of_char m true, true, (if b = 'B' then 8. else 1.)
+        | m::'i'::b::[] -> size, multiplier_of_char m false, false, (if b = 'B' then 8. else 1.)
+        | _ -> assert false
       in
-      { size = size; multiplier = multiplier; base10 = base; unit = unit }
+      { size = size; multiplier = multiplier; base10 = base10; unit = unit }
     else
       raise (Invalid_size_description n)
-    
+
 let compare n1 n2 =
   let v1 = value n1 in
   let v2 = value n2 in
@@ -106,5 +108,11 @@ let sub s1 s2 =
   match s2 with
     | None -> s1
     | Some s2 ->
-        let v = max 0. (value s1 -. value s2) in
-        make (string_of_float v)
+       if s1.multiplier = s2.multiplier
+          && s1.base10 = s2.base10
+          && s1.unit = s2.unit
+       then
+         { s1 with size = max 0. (s1.size -. s2.size) }
+       else
+         let v = max 0. (value s1 -. value s2) in
+         make (string_of_float v)
